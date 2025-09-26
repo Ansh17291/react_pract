2ea3e21bd1eec94923f8291261ecfdb52525526d
Ansh <ansh2shweta@gmail.com>
2025-09-27 00:37:56 +0530

    Changed names of file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import Greet from "./Create_Components/Greet";
-import Header from "./Getting_Comfortable_with_componets/Header";
-import Main from "./Getting_Comfortable_with_componets/MainCont";
-import Footer from "./Getting_Comfortable_with_componets/Footer";
-import JSXRules from "./JSX_Rules/JSXRules";
+import Greet from "./2_Create_Components/Greet";
+import Header from "./3_Getting_Comfortable_with_componets/Header";
+import Main from "./3_Getting_Comfortable_with_componets/MainCont";
+import Footer from "./3_Getting_Comfortable_with_componets/Footer";
+import JSXRules from "./1_JSX_Rules/JSXRules";
 import Add from "./Adding_JS/Add";
 import MoreFun from "./Adding_JS/MoreFun";
 const App = () => { 
